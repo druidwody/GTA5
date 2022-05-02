01937e373913a38442a917eeaa7c35954054ee79
druidwody <itahi4@gmail.com>
2022-05-02 22:14:09 +0200

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { GaugeModule} from 'angular-gauge';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +21,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    GaugeModule.forRoot(),
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
