26fdeaedfa6339200bee3f4c9e1f2b6f007e1991
druidwody <itahi4@gmail.com>
2022-07-18 20:05:00 +0200

    t

--- src/app/components/search-bar/search-bar.component.spec.ts
+++ src/app/components/search-bar/search-bar.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/**import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchBarComponent } from './search-bar.component';
 
@@ -23,3 +23,4 @@ describe('SearchBarComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+**/
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/**import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -12,4 +12,4 @@ export class SearchBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-}
+}**/
